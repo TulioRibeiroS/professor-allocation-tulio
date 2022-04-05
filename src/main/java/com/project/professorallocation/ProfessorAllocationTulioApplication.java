14e7e57e7037413eac3b4531a3cc2cd07c9c1a48
Túlio Ribeiro <tulioawake@gmail.com>
2022-04-05 19:49:19 -0300

    Add initial SpringBoot Web project files

---
+++ src/main/java/com/project/professorallocation/ProfessorAllocationTulioApplication.java
@@ -0,0 +1,13 @@
+package com.project.professorallocation;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProfessorAllocationTulioApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProfessorAllocationTulioApplication.class, args);
+	}
+
+}
